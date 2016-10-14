86e2f977bf0f2e2ee35d20d5b0e09986b9bcbdb1
Norckon <dfc643@acgdraw.com>
2016-10-15 00:01:28 +0800

    fix bugs

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-##google in heroku
+## 简易谷歌反代（Heroku版）
 
----
-有时候访问google不便，需要这么一个逗比的东西
+部署在 Heroku 中即可立即获得一个非常方便访问谷歌的方式。源代码基于 @Hsiny 的 ```google-in-heroku``` 修改而来。
 
-可以直接部署到heroku中
+### 如何部署
+
+在 Windows 的 Git Bash 或者 Linux 的终端中运行如下命令即可。
 
 ```bash
-git clone https://github.com/Hsiny/google-in-heroku.git
+git clone https://github.com/brcm/google-in-heroku
 cd google-in-heroku
 heroku create
 git add -A
@@ -14,4 +15,17 @@ git commit -m "init"
 git push heroku master
 heroku open
 ```
-搞定
+
+### 更新内容
+
+1. 修复 User-Agent 识别错误致访问到旧版谷歌
+2. 修复中文搜索时，搜索结果页出现乱码问题
+3. 强制设置默认搜索语言为简体中文
+4. 强行关闭谷歌的安全搜索选项，可搜索额外内容
+5. 强行移除多余 Javascript 脚本，例如 G+ 社区、广告等
+6. 完善 HTTP 请求头，HTTP 响应头添加缓存字段
+
+### 已知问题
+
+1. 图片搜索无法点击查看，无法翻页加载
+2. 谷歌的首页和搜索页面的 Google 标志加载慢
--- hello.py
+++ hello.py
@@ -1,35 +1,103 @@
+# coding=utf-8
 import os
+import sys
 from wsgiref.simple_server import make_server
 import urllib2
+reload(sys)   
+sys.setdefaultencoding('utf-8')
+
 def application(environ,start_response):
 
-    googleUrl="https://www.google.com"
+    sourceUrl = "https://www.google.com"
 
     try:
-        userAgent=str(environ["HTTP_USER_AGENT"])
-        requestUrl=str(environ["PATH_INFO"])+"?"+str(environ["QUERY_STRING"])
-        httpAccept=str(environ["HTTP_ACCEPT"]) 
+        requestUrl = str(environ["PATH_INFO"])+"?"+str(environ["QUERY_STRING"])
+        httpHost = str(environ["HTTP_HOST"])
+        try:
+            httpAccept = str(environ["HTTP_ACCEPT"])
+        except:
+            httpAccept = ""
+        try:
+            userAgent = str(environ["HTTP_USER_AGENT"])
+        except:
+            userAgent = ""
+        try:
+            httpCookie = str(environ["HTTP_COOKIE"])
+        except:
+            httpCookie = ""
+        try:
+            httpLang = str(environ["HTTP_ACCEPT_LANGUAGE"])
+        except:
+            httpLang = ""
+        try:
+            httpEncode = str(environ["HTTP_ACCEPT_ENCODING"])
+        except:
+            httpEncode = ""
+        try:
+            bodyLength = int(environ.get('CONTENT_LENGTH', '0'))
+        except ValueError:
+            bodyLength = 0
     except:
-        userAgent="Mozilla/5.0"
-        requestUrl=""
-
+        requestUrl = ""
+    
+    # Google Change Options
+    if requestUrl == "/?":
+        requestUrl = "/?hl=zh-CN"
+    requestUrl = requestUrl.replace('safe=strict','safe=off')
+    
+    print requestUrl
+    
+    # Advs block listener
+    if str(environ["PATH_INFO"]) == '/cdn-dat/adb':
+        html = ""
+        status = '200 OK'
+        response_headers  =  [('Content-Type', 'text/html;charset=utf-8'),('Cache-Control', 'public, max-age=2592000'),('Content-Length', str(len(html)))]
+        start_response(status, response_headers)
+        return html
 
     try:
-        opener=urllib2.build_opener()
-        opener.addheaders=[('User-agent', userAgent)]
-        opener.addheaders=[('Accept',httpAccept)]
-        html=opener.open(googleUrl+requestUrl).read()
-    except:
-        html="Can not open the page.Please retry in a few minutes"
+        opener = urllib2.build_opener()
+        opener.addheaders = [('Accept',httpAccept)]
+        opener.addheaders = [('Accept-Encoding',httpEncode)]
+        opener.addheaders = [('Accept-Language',httpLang)]
+        opener.addheaders = [('Cookie',httpCookie)]
+        opener.addheaders = [('User-Agent', userAgent)]
+        if bodyLength != 0:
+            body = environ['wsgi.input'].read(bodyLength)
+            html = opener.open(sourceUrl+requestUrl, body).read()
+        else:
+            html = opener.open(sourceUrl+requestUrl).read()
 
-    status='200 OK'
-    response_headers = [('Content-Type', 'text/html;charset=utf-8'),('Content-Length', str(len(html)))]
+        # Replace the blocked resource
+        html = html.replace('https://ajax.googleapis.com/ajax/libs/jquery/','//cdn.bootcss.com/jquery/')
+        html = html.replace('https://ajax.googleapis.com/ajax/libs/angularjs/','//cdn.bootcss.com/angular.js/')
+        html = html.replace('Google.com in English','RazerNiz Forwarder 1.0')
+        html = html.replace(sourceUrl,'//' + httpHost)
+        
+        # Filter Advs Javascripts
+        html = html.replace('//imasdk.googleapis.com/','/cdn-dat/adb?')
+        html = html.replace('/xjs/_/js/','/cdn-dat/adb?')
+        html = html.replace('//pagead2.googlesyndication.com/','/cdn-dat/adb?')
+        html = html.replace('//partner.googleadservices.com/','/cdn-dat/adb?')
+        html = html.replace('//www.googletagservices.com/','/cdn-dat/adb?')
+        html = html.replace('//www.google-analytics.com/','/cdn-dat/adb?')
+        html = html.replace('//apis.google.com','/cdn-dat/adb?')
+        html = html.replace('//plus.google.com/','/cdn-dat/adb?')
+        html = html.replace('//ogs.google.com/','/cdn-dat/adb?')
+        html = html.replace('//client5.google.com/','/cdn-dat/adb?')
+        html = html.replace('//client4.google.com/','/cdn-dat/adb?')
+        html = html.replace('www.gstatic.com','www.baidu.com')
+        
+        status = '200 OK'
+    except:
+        html = "<html><head><title>Forwarder Unavailable</title></head><body><center><h1>Forwarder Unavailable</h1><hr/>RazerNiz Forwarder 1.0</center></body></html>"
+        status = '503 Forwarder Unavailable'
+    
+    response_headers  =  [('Content-Type', 'text/html; charset=UTF-8'),('Cache-Control', 'public, max-age=86400'),('Content-Length', str(len(html)))]
     start_response(status, response_headers)
 
     return html
 
-port=int(os.environ.get("PORT",5000))
-httpd=make_server('0.0.0.0',port,application)
+port = int(os.environ.get("PORT",5000))
+httpd = make_server('0.0.0.0',port,application)
 httpd.serve_forever()
-
-
